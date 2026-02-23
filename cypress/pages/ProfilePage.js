5924139f3a9ef3c1819f10aa8608b031cde9e7f7
Edward <edward1791c@gmail.com>
2026-02-22 22:25:41 -0300

    feat(registration): automate user registration flow and form validations

---
+++ cypress/pages/ProfilePage.js
@@ -0,0 +1,40 @@
+class ProfilePage {
+
+    // Selectors (Getters)
+    get profileTitle() {
+        return cy.get('[data-testid="profile-title"]');
+    }
+
+    get logoutNav() {
+        return cy.get('[data-testid="logout-nav"]');
+    }
+
+    get profileName() {
+        return cy.get('[data-testid="profile-name"]');
+    }
+
+    get profileEmail() {
+        return cy.get('[data-testid="profile-email"]');
+    }
+
+    get logoutButton() {
+        return cy.get('[data-testid="logout-btn"]');
+    }
+
+    // Validations
+    verifyPageElements() {
+        this.profileTitle.should('be.visible');
+        this.logoutNav.should('be.visible');
+        this.profileName.should('be.visible');
+        this.profileEmail.should('be.visible');
+        this.logoutButton.should('be.visible');
+    }
+
+    // Actions (Methods)
+    logout() {
+        this.logoutButton.click();
+    }
+
+}
+
+export default new ProfilePage();
